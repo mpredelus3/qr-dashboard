4e81a1130fe50123f80cdbe9e72c8e35f707d45c
mpredelus3 <mpredelus3@gmail.com>
2024-09-01 19:02:18 -0700

    correct ms sql installed/debugging sql

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-// server/index.js
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,9 +7,8 @@ const mssql = require('mssql');
 const axios = require('axios');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
